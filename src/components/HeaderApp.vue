64594b68b445ba9dd373eb1b842ab80005ba305f
nguyenthanh1995 <Thanhnguyennguyen1995@gmail.com>
2020-08-19 15:22:36 +0700

    fix login

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -5,7 +5,7 @@
          Free iOS Admin
       </b-navbar-brand>
       <b-navbar-brand tag="span">
-         <S-SDropdown>
+         <S-SDropdown class="no-caret" v-if="$store.account">
             <template v-slot:button-content>
                <img src="https://nguyenthanh1995.github.io/favicon.ico" class="avatar"/>
             </template>
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -5,8 +5,8 @@
          }" @submit.prevent="login">
          <img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
          <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-         <input type="username" class="form-control" placeholder="Email address" required autofocus>
-         <input type="password" class="form-control" placeholder="Password" required>
+         <input type="text" name="username" class="form-control" placeholder="Email address" required autofocus>
+         <input type="password" name="password" class="form-control" placeholder="Password" required>
          <div class="checkbox mb-3">
             <label>
                <input type="checkbox" value="remember-me"> Remember me
